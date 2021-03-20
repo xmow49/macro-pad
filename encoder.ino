94d19ed50aede17ba6c501c87dc3e215176f73b0
Dorian Benech <47485034+xmow49@users.noreply.github.com>
2021-03-20 23:32:53 +0100

    set key action with serial
    exemple: set key 2 ConsumerCalculator

--- encoder.ino
+++ encoder.ino
@@ -1,14 +1,71 @@
 #include "HID-Project.h"
 
+const int MediaFastForward = 0xB3;
+const int MediaRewind = 0xB4;
+const int MediaNext = 0xB5;
+const int MediaPrevious = 0xB6;
+const int MediaStop = 0xB7;
+const int MediaPlayPause = 0xCD;
+
+const int MediaVolumeMute = 0xE2;
+const int MediaVolumeUP = 0xE9;
+const int MediaVolumeDOWN = 0xEA;
+
+const int ConsumerEmailReader = 0x18A;
+const int ConsumerCalculator = 0x192;
+const int ConsumerExplorer = 0x194;
+
+const int ConsumerBrowserHome = 0x223;
+const int ConsumerBrowserBack = 0x224;
+const int ConsumerBrowserForward = 0x225;
+const int ConsumerBrowserRefresh = 0x227;
+const int ConsumerBrowserBookmarks = 0x22A;
+
+
+String strAction[] = {"MediaFastForward", "MediaRewind", "MediaNext", "MediaPrevious", "MediaStop", "MediaPlayPause",
+                      "MediaVolumeMute", "MediaVolumeUP", "MediaVolumeDOWN",
+                      "ConsumerEmailReader", "ConsumerCalculator", "ConsumerExplorer",
+                      "ConsumerBrowserHome", "ConsumerBrowserBack", "ConsumerBrowserForward", "ConsumerBrowserRefresh", "ConsumerBrowserBookmarks"
+                     };
+int hexAction[] = {0xB3, 0xB4, 0xB5, 0xB6, 0xB7, 0xCD,
+                   0xE2, 0xE9, 0xEA,
+                   0x18A, 0x192, 0x194,
+                   0x223, 0x224, 0x225, 0x227, 0x22A
+                  };
+
+int keyAction[3] = {0,0,0};
+
 const int PinA = 7;
-const int PinB = 6;
+//const int PinB = 6;
 
-const int PinSW = 8;
-
+const int PinB1 = 6;
+const int PinB2 = 5;
+const int PinB3 = 4;
 
 int lastCount = 50;
 volatile int virtualPosition = 50;
 
+String serialMsg;
+
+String getArgs(String data, char separator, int index)
+{
+  //Function for separate String with special characters
+  int found = 0;
+  int strIndex[] = {0, -1};
+  int maxIndex = data.length() - 1;
+
+  for (int i = 0; i <= maxIndex && found <= index; i++)
+  {
+    if (data.charAt(i) == separator || i == maxIndex)
+    {
+      found++;
+      strIndex[0] = strIndex[1] + 1;
+      strIndex[1] = (i == maxIndex) ? i + 1 : i;
+    }
+  }
+
+  return found > index ? data.substring(strIndex[0], strIndex[1]) : "";
+}
 
 void isr ()  {
   static unsigned long lastInterruptTime = 0;
@@ -16,12 +73,13 @@ void isr ()  {
 
   // If interrupts come faster than 5ms, assume it's a bounce and ignore
   if (interruptTime - lastInterruptTime > 5) {
-    if (digitalRead(PinB) == LOW)
+
+    if (analogRead(A0) == 0)
     {
-      virtualPosition-- ; // Could be -5 or -10
+      virtualPosition = virtualPosition - 10 ; // Could be -5 or -10
     }
     else {
-      virtualPosition++ ; // Could be +5 or +10
+      virtualPosition = virtualPosition + 10 ; // Could be +5 or +10
     }
 
     // Restrict value from 0 to +100
@@ -39,39 +97,69 @@ void setup() {
 
   // Rotary pulses are INPUTs
   pinMode(PinA, INPUT);
-  pinMode(PinB, INPUT);
+  //pinMode(PinB, INPUT);
 
-  // Switch is floating so use the in-built PULLUP so we don't need a resistor
-  pinMode(PinSW, INPUT_PULLUP);
+  pinMode(PinB1, INPUT);
+  pinMode(PinB2, INPUT);
+  pinMode(PinB3, INPUT);
 
-  // Attach the routine to service the interrupts
   attachInterrupt(digitalPinToInterrupt(PinA), isr, LOW);
-
-  // Ready to go!
   Serial.println("Start");
-  //Keyboard.begin();
   Consumer.begin();
 
 }
 
 void loop() {
-
-
-  // If the current rotary switch position has changed then update everythinguuuuuuuu
+  if (digitalRead(PinB1)) {
+    while (digitalRead(PinB1)) {}
+    Consumer.write(keyAction[0]);
+  }
+  if (digitalRead(PinB2)) {
+    while (digitalRead(PinB2)) {}
+    Consumer.write(keyAction[1]);
+  }
+  if (digitalRead(PinB3)) {
+    while (digitalRead(PinB3)) {}
+    Consumer.write(keyAction[2]);
+  }
   if (virtualPosition != lastCount) {
-
     // Write out to serial monitor the value and direction
-    Serial.print(virtualPosition > lastCount ? "Up  :" : "Down:");
-    Serial.println(virtualPosition);
+    //Serial.print(virtualPosition > lastCount ? "Up  :" : "Down:");
+    //Serial.println(virtualPosition);
 
     if (virtualPosition > lastCount) {
+      Serial.println("Encoder1:UP");
       Consumer.write(MEDIA_VOL_UP);
     } else {
       Consumer.write(MEDIA_VOL_DOWN);
+      Serial.println("Encoder1:DOWN");
     }
-
-    // Keep track of this new valueuuu
     lastCount = virtualPosition ;
   }
 
+  if (Serial.available() > 0) {
+    serialMsg = Serial.readString();
+    serialMsg.trim();
+    if (serialMsg.indexOf("set") > -1) {
+      if (serialMsg.indexOf("key") > -1) {
+        int key = getArgs(serialMsg, ' ', 2).toInt();
+        String action = getArgs(serialMsg, ' ', 3);
+        keyAction[key-1] = getAction(action);
+      }
+    }
+  }
+
+}
+
+
+int getAction(String action) {
+  for (byte i = 0; i < sizeof(strAction); i++) {
+    if (action == strAction[i]) {
+      Serial.print("Found: ");
+      Serial.println(i);
+      return hexAction[i];
+      break;
+    }
+  }
+return 0;
 }
